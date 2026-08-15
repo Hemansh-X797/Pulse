dc4a3d8431351e59d25981ab1031037179127aed
Hemansh-X797 <kundansmishra@gmail.com>
2026-08-15 18:39:13 +0530

    refactor(shell): update NotificationsPanel component

--- web/src/components/shell/NotificationsPanel.tsx
+++ web/src/components/shell/NotificationsPanel.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { useEffect } from 'react';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { listNotifications, markNotificationRead, markAllNotificationsRead } from '../../lib/api/notifications';
