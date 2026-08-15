3ebd94ecc7cae04d7416d6d23a30acb6005c6f00
Hemansh-X797 <kundansmishra@gmail.com>
2026-08-15 18:39:08 +0530

    refactor(shell): update AppShell component

--- web/src/components/shell/AppShell.tsx
+++ web/src/components/shell/AppShell.tsx
@@ -1,15 +1,15 @@
-import { Outlet } from '@tanstack/react-router';
+'use client';
+
+import type { ReactNode } from 'react';
 import { GlobalNav } from './GlobalNav';
 import { SecondarySidebar } from './SecondarySidebar';
 
-export function AppShell() {
+export function AppShell({ children }: { children: ReactNode }) {
   return (
     <div className="flex h-screen w-full bg-neutral-950 text-neutral-50">
       <GlobalNav />
       <SecondarySidebar />
-      <main className="flex min-w-0 flex-1 flex-col">
-        <Outlet />
-      </main>
+      <main className="flex min-w-0 flex-1 flex-col">{children}</main>
     </div>
   );
 }
